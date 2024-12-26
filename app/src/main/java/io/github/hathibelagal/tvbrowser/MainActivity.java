cb31918206fa229e2ffccfadba4912546d16f8f4
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2024-12-27 03:26:33 +0530

    Navigation

--- app/src/main/java/io/github/hathibelagal/tvbrowser/MainActivity.java
+++ app/src/main/java/io/github/hathibelagal/tvbrowser/MainActivity.java
@@ -1,14 +1,22 @@
 package io.github.hathibelagal.tvbrowser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,9 +41,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                status.setText(getResources().getText(R.string.status_complete));
+                status.setVisibility(View.INVISIBLE);
             }
 
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                status.setVisibility(View.VISIBLE);
+            }
         });
         browser.setWebChromeClient(new WebChromeClient() {
             @Override
@@ -44,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.setTitle(title);
             }
 
+            @SuppressLint("SetTextI18n")
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
                 super.onProgressChanged(view, newProgress);
@@ -54,5 +68,32 @@ public class MainActivity extends AppCompatActivity {
 
         });
         browser.loadUrl(HOMEPAGE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.browser_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        CharSequence title = Objects.requireNonNull(item.getTitle());
+        if(title.equals(getResources().getText(R.string.home))) {
+            browser.loadUrl(HOMEPAGE);
+        }
+        if(title.equals(getResources().getText(R.string.back))) {
+            browser.goBack();
+        }
+        if(title.equals(getResources().getText(R.string.forward))) {
+            browser.goForward();
+        }
+        if(title.equals(getResources().getText(R.string.refresh))) {
+            browser.reload();
+        }
+        if(title.equals(getResources().getText(R.string.quit))) {
+            MainActivity.this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
