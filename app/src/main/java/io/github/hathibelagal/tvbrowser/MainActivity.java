54abe96d2d83f6d1135c4877b8c381c5fd190c05
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2024-12-27 22:10:30 +0530

    Cookies and DOM storage

--- app/src/main/java/io/github/hathibelagal/tvbrowser/MainActivity.java
+++ app/src/main/java/io/github/hathibelagal/tvbrowser/MainActivity.java
@@ -10,8 +10,10 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -21,6 +23,7 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
     private static final String HOMEPAGE = "https://hathibelagal-dev.github.io/tv/home.html";
+    private static final String UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:133.0) Gecko/20100101 Firefox/133.0";
     WebView browser;
     TextView status;
 
@@ -32,7 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
         browser = findViewById(R.id.browser);
         status = findViewById(R.id.browser_status);
-        browser.getSettings().setJavaScriptEnabled(true);
+
+        WebSettings settings = browser.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setUserAgentString(UA);
+        settings.setDomStorageEnabled(true);
+
+        CookieManager.getInstance().setAcceptCookie(true);
+
         browser.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
